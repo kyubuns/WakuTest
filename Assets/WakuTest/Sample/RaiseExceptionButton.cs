54d56f79cc3d6b5301369067809355903c3ec427
kyubuns <kyubuns@gmail.com>
2017-06-17 18:00:34 +0900

    RaiseExceptionButton

---
+++ Assets/WakuTest/Sample/RaiseExceptionButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WakuTest.Sample
+{
+	[RequireComponent(typeof(Button))]
+	public class RaiseExceptionButton : MonoBehaviour
+	{
+		void Start()
+		{
+			GetComponent<Button>().onClick.AddListener(() =>
+			{
+				throw new Exception("click raise exception button");
+			});
+		}
+	}
+}
