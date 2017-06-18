a19985eef614067a47958126a265cbbf43791ee4
kyubuns <kyubuns@gmail.com>
2017-06-18 14:13:14 +0900

    unitypackge

---
+++ Assets/Editor/PackageExporter.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public static class PackageExporter
+{
+	[MenuItem("Export/Export Unity Package")]
+	public static void ExportUnityPackage()
+	{
+		var libraryDirectories = new[] {
+			"Assets/WakuTest"
+		};
+
+		var outputFilePath = Path.Combine(Path.GetDirectoryName(Application.dataPath), "WakuTest.unitypackage");
+
+		Debug.LogFormat("Export yokan package to {0}", outputFilePath);
+
+		AssetDatabase.ExportPackage(libraryDirectories, outputFilePath, ExportPackageOptions.Recurse);
+
+		Debug.LogFormat("Success");
+	}
+}
